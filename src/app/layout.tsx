b0656e3599427288ee5db07c5d9c7f610e639604
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-12-11 18:28:06 +0545

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/utils"
+import type { Metadata } from "next"
+import { EB_Garamond, Inter } from "next/font/google"
+import { Providers } from "./components/providers"
+
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const eb_garamond = EB_Garamond({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
+
+export const metadata: Metadata = {
+  title: "NextPing",
+  description: "Created using jStack",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
+      <body className="font-sans bg-brand-50 text-brand-950 antialiased">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
